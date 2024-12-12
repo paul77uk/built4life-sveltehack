1faac983b072e018a0476703cf1f0d8d2e024f26
paul77uk <paul77uk@gmail.com>
2024-12-12 18:40:49 +0000

    my programs page, tabs seems to be working

--- src/routes/my-programs/+page.svelte
+++ src/routes/my-programs/+page.svelte
@@ -1,10 +1,12 @@
 <script>
+	import * as Tabs from '$lib/components/ui/tabs/index';
+	import * as Card from '$lib/components/ui/card/index';
 	import { useMyProgramState } from '$lib/state/my-program-state.svelte';
 
 	let programState = useMyProgramState();
 </script>
 
-<main>
+<!-- <main>
 	<h1>My Programs</h1>
 	<ul>
 		<li>{programState?.program?.title}</li>
@@ -25,4 +27,52 @@
 			{/each}
 		{/if}
 	</ul>
-</main>
+</main> -->
+
+<Tabs.Root value={programState.program?.weeks[0].title} class="w-[400px] mx-auto mt-3">
+	<Tabs.List class="grid w-full grid-cols-2 my-3">
+		{#if programState.program}
+			{#each programState.program.weeks as week (week.title)}
+				<Tabs.Trigger value={week.title}>
+					{week.title}
+				</Tabs.Trigger>
+			{/each}
+		{/if}
+	</Tabs.List>
+	{#if programState.program}
+		{#each programState.program.weeks as week (week.title)}
+			<Tabs.Content value={week.title}>
+				<Tabs.Root value={week.days[0].title} class="w-[400px]">
+					<Tabs.List class="grid w-full grid-cols-2 my-3">
+						{#each week.days as day (day.title)}
+							<Tabs.Trigger value={day.title}>
+								{day.title}
+							</Tabs.Trigger>
+						{/each}
+					</Tabs.List>
+					{#each week.days as day (day.title)}
+						<Tabs.Content value={day.title}>
+							{#each day.daysToWorkouts as dayToWorkout}
+								<Card.Root class="my-5">
+									<Card.Header>
+										<Card.Title class="text-center mb-2">{dayToWorkout.workout.title}</Card.Title>
+									</Card.Header>
+									<Card.Description class="text-center"
+										>{dayToWorkout.workout.description}</Card.Description
+									>
+									<Card.Content class="text-center">
+										{#if dayToWorkout.workout.exercises}
+											{#each dayToWorkout.workout.exercises as exercise}
+												<p>{exercise}</p>
+											{/each}
+										{/if}
+									</Card.Content>
+								</Card.Root>
+							{/each}
+						</Tabs.Content>
+					{/each}
+				</Tabs.Root>
+			</Tabs.Content>
+		{/each}
+	{/if}
+</Tabs.Root>
